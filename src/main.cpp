7ab09b9322298a72bfad34f652fd69e8b15ee872
RobertNagy299 <nagyrobertofficial@gmail.com>
2025-07-20 05:40:59 -0400

    Test DBEngine linking

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
 #include "parser/parser.h"
+#include "DBEngine/engine.h"
 
 int main() {
     std::cout << "hi!\n" << __cplusplus << '\n';
     auto myParser = SQLParser();
+    auto Engine = DataBaseEngine();
+
+    Engine.initDBEngine();
     myParser.initParser();
 
     return 0;
