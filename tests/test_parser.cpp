383d3c660754feb10d34621269310a013be8d6f9
RobertNagy299 <nagyrobertofficial@gmail.com>
2025-10-04 08:07:06 -0400

    Add new test case for a complete untyped table creation with column modifiers, defaults, etc.

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -7,9 +7,32 @@ extern ASTNode *root;
 extern FILE *yyin;
 extern int yylex_destroy();
 
-TEST(ParserTest, CreateTableCorrectly)
+TEST(ParserTest, CreateUntypedTableCorrectly)
 {
   const char *sql = "CREATE UNTYPED TABLE users(id, name, age);";
+
+  // Open SQL string as input
+  FILE *f = fmemopen((void *)sql, strlen(sql), "r");
+  ASSERT_NE(f, nullptr);
+
+  yyin = f;
+  root = nullptr;
+
+  int result = yyparse();
+  yylex_destroy();
+  fclose(f);
+
+  ASSERT_EQ(result, 0);
+  ASSERT_NE(root, nullptr);
+
+  // Cleanup
+  delete root;
+  root = nullptr;
+}
+
+TEST(ParserTest, CreateUntypedTableCorrectlyWithColModifiers)
+{
+  const char *sql = "CREATE UNTYPED TABLE users(id PRIMARY KEY, name NOT NULL DEFAULT 'Gipsz jakab', age DEFAULT 18);";
 
   // Open SQL string as input
   FILE *f = fmemopen((void *)sql, strlen(sql), "r");
